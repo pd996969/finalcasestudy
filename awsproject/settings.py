5ad14eb4b03b15aaa78ed87f8be8449ae5ea7487
pd996969 <55734713+pd996969@users.noreply.github.com>
2019-09-29 16:13:56 +0530

    Update settings.py

--- awsproject/settings.py
+++ awsproject/settings.py
@@ -4,7 +4,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY= os.environ['SECRET_KEY']
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    SECRET_KEY = 'nu^td3h_fgl2!mf&+6#f*4!^5a_=axg1m%w41#7ps=074*w&_c'
 
 DEBUG = True
 
